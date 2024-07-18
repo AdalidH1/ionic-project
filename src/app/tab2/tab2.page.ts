a140a798a4aff83a0c9d0a3bccb52b1ec935b79c
AdalidH1 <adalidh141@gmail.com>
2024-07-18 14:28:24 -0700

    camera

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page  {
+  photos: any[] = []
+  constructor(public photo:PhotoService) {
+    this.photos = this.photo.photos
+  }
 
-  constructor() {}
+  takePhoto() {
+    this.photo.addPhoto()
+  }
+
+
 
 }
