10735fd5866b7f5864e8aa7f76cede5b5848beff
AdalidH1 <adalidh141@gmail.com>
2024-08-06 14:26:39 -0700

    firebase storage

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { AngularFireStorage } from "@angular/fire/compat/storage";
+import { ref, getDownloadURL, listAll, getStorage, uploadString } from "firebase/storage";
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +9,36 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 export class PhotoService {
   public photos:any[] = []
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage) { 
+    // this.getImages()
+  }
 
   public async addPhoto() {
     const photo = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
+      resultType: CameraResultType.DataUrl,
       source: CameraSource.Camera,
       quality: 100
     })
-    this.photos.push(photo.webPath)
+
+    let imagePath = "unit-3-photo" + Date.now()
+
+    let image = await this.uploadImage(imagePath,photo.dataUrl)
+
+
+    this.photos.push(image)
+  }
+
+  async uploadImage(path: string, data_url: any) {
+    const storage = getStorage()
+    const storageRef = ref(storage, path)
+
+    return uploadString(storageRef, data_url, 'data_url').then(() => {
+      return getDownloadURL(storageRef)
+    })
+  }
+
+  async getImages() {
+    const storage = getStorage()
   }
 
 }
